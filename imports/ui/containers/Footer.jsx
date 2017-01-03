3aec8e6b49be5e849632fa9bceed72019fcd0ff0
Kristjan Patrekur <Krillinn@gmail.com>
2017-01-03 14:47:58 +0000

    FooterItem deleted

--- imports/ui/containers/Footer.jsx
+++ imports/ui/containers/Footer.jsx
@@ -65,7 +65,6 @@ const definitionLists = [
 ];
 
 class Footer extends React.Component {
-
   renderDefinitionList({title, definitionDescriptions}, index) {
     return (
       <FooterDefinitionList
